f0d7bb3ac61a3fdf6cb01d199d99f0cb545abe32
[hamna shaikh] <[hirasheikh556@gmail.com]>
2024-05-23 06:26:13 +0500

    tweak the text and added console at the end

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 
 const asnwer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondNumber" },
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
   {
     message: "Select one the operator to perform action",
     type: "list",
